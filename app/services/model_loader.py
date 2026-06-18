a86c2d6d03ddcf6f1f4724746b75de458a19d08e
Sharif Abusad <abusharif866@gmail.com>
2026-06-18 11:25:31 +0530

    Remove unnecessary Codes

--- app/services/model_loader.py
+++ app/services/model_loader.py
@@ -26,79 +26,10 @@ import tensorflow as tf
 
 from tensorflow.keras.models import load_model
 from app.config import LSTM_META, MODEL_LSTM, SCALER_PATH
-# ──────────────────────────────────────────────────────────
-# Project Base Directory
-# ──────────────────────────────────────────────────────────
-# BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
-# ──────────────────────────────────────────────────────────
-# Model Paths
-# ──────────────────────────────────────────────────────────
-# MODEL_PATH = (
-#     BASE_DIR /
-#     "models" /
-#     "lstm" /
-#     "lstm_temp_model.keras"
-# )
-
-# SCALER_PATH = (
-#     BASE_DIR /
-#     "models" /
-#     "scalers" /
-#     "lstm_temp_scaler.pkl"
-# )
-
-# META_PATH = (
-#     BASE_DIR /
-#     "models" /
-#     "lstm" /
-#     "model_meta.json"
-# )
-
-# ──────────────────────────────────────────────────────────
-# Custom TensorFlow Functions
-# Required for loading Lambda layers safely
-# ──────────────────────────────────────────────────────────
-# def squeeze_last_dim(x):
-#     """
-#     Remove the final dimension from tensor.
-
-#     Used inside custom Lambda layer during
-#     model loading.
-#     """
-
-#     return tf.squeeze(x, axis=-1)
-
-
-# def squeeze_output_shape(input_shape):
-#     """
-#     Define output shape after squeeze operation.
-
-#     Args:
-#         input_shape : Tensor input shape
-
-#     Returns:
-#         Tuple representing squeezed shape
-#     """
-
-#     return (
-#         input_shape[0],
-#         input_shape[1]
-#     )
 
 # ──────────────────────────────────────────────────────────
 # # Load Trained LSTM Model
 # # ──────────────────────────────────────────────────────────
-# loaded_lstm_model = load_model(
-#     # MODEL_PATH,
-#     MODEL_LSTM,
-#     custom_objects={
-#         "squeeze_last_dim": squeeze_last_dim,
-#         "squeeze_output_shape": squeeze_output_shape,
-#     },
-
-#     safe_mode=False,
-# )
 loaded_lstm_model = load_model(
     MODEL_LSTM,
     compile=False
